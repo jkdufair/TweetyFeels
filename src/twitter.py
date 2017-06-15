14cd045382ffca3fe98670e39910f828fc8a40a2
Jason Dufair <jase@purdue.edu>
2017-06-15 13:13:23 -0400

    Write buffer to file. Monitor status at terminal

--- src/twitter.py
+++ src/twitter.py
@@ -1,25 +1,47 @@
 """Functions to fetch or stream tweets"""
 
 import csv
+import re
+import time
+from concurrent.futures import ThreadPoolExecutor
+from collections import deque
 import tweepy
 import twitter_auth
-import re
 
-tweet_buffer = []
+
+TWEET_QUEUE = deque()
+
+
+class TweetCounter():
+    """Class for managing the global tweet count"""
+
+    def __init__(self):
+        self.tweet_count = 0
+
+    def increment(self):
+        """Bump it up, baby"""
+        self.tweet_count = self.tweet_count + 1
+
+    def get_tweet_count(self):
+        """Return the tweet count. Duh"""
+        return self.tweet_count
 
 
 class StreamListener(tweepy.StreamListener):
     """Class to listen for twitter streams and handle them"""
 
-    def __init__(self, writerFunction):
+    def __init__(self, writerFunction, tweet_counter):
         """Store the function that writes out the tweet data"""
         self.writer = writerFunction
+        self.tweet_counter = tweet_counter
         super(StreamListener, self).__init__()
 
     def on_status(self, status):
         self.writer(status)
+        self.tweet_counter.increment()
 
     def on_error(self, status_code):
+        print('hello')
         if status_code == 420:
             return False
 
@@ -49,10 +71,7 @@ def get_tweets(query):
 
 def write_status(status, writer):
     """Write the details of the tweet to the csv writer"""
-    writer.writerow({'created_at': status.created_at,
-                     'text': status.text,
-                     'user_id': status.user.id,
-                     'id': status.id})
+    writer.writerow(status)
 
 
 def queue_status(status):
@@ -62,24 +81,49 @@ def queue_status(status):
                             re.sub(r'\s+', ' ', status.text.strip())),
                         'user_id': status.user.id,
                         'id': status.id}
-    tweet_buffer.append(processed_status)
-    print(processed_status)
+    TWEET_QUEUE.append(processed_status)
 
 
-def stream_tweets(keywords):
+def stream_tweets(keywords, tweet_counter):
     """Stream tweets about bitcoin to csv file"""
+    print('stream_tweets() started')
     auth = twitter_auth.authenticate()
 
-    # csvfile = open('data/tweet_stream.csv', 'a')
-    # field_names = ['created_at', 'text', 'user_id', 'id']
-    # writer = csv.DictWriter(csvfile, fieldnames=field_names)
-
-    listener = StreamListener(writerFunction=queue_status)
+    listener = StreamListener(queue_status, tweet_counter)
     stream = tweepy.Stream(auth=auth, listener=listener)
     stream.filter(track=keywords, async=True)
 
 
-# get_tweets("bitcoin since:2017-06-11 until:2017-06-12")
-# executor = ThreadPoolExecutor(max_workers=2)
+def write_statuses_from_buffer():
+    """Take the status objects from the tweet buffer and append them to the csv"""
+    print('write_statuses_from_buffer() started')
+    while True:
+        time.sleep(60)
+        with open('data/tweet_stream.csv', 'a') as csv_file:
+            field_names = ['created_at', 'text', 'user_id', 'id']
+            writer = csv.DictWriter(csv_file, fieldnames=field_names)
+            while TWEET_QUEUE:
+                write_status(TWEET_QUEUE.popleft(), writer)
 
-stream_tweets(['bitcoin'])
+
+def monitor_status(tweet_counter):
+    """Print a message to the console with queue length and total tweets written"""
+    print('monitor_status() started')
+    while True:
+        time.sleep(1)
+        queue_length = len(TWEET_QUEUE)
+        print('                                                                ', end='\r')
+        print('Queue length: {}\t\tTotal tweets collected: {}'.format(
+            queue_length, tweet_counter.get_tweet_count()), end='\r')
+
+
+def start():
+    """Kick off twitter streaming process and file writing process"""
+    tweet_counter = TweetCounter()
+    with ThreadPoolExecutor(max_workers=3) as executor:
+        executor.submit(stream_tweets, ['bitcoin'], tweet_counter)
+        executor.submit(write_statuses_from_buffer)
+        executor.submit(monitor_status, tweet_counter)
+
+
+start()
